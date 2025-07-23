d1b4a46180e37302bf30c1f36303cb973b2d36fc
CarlosOrtegaDoubleV <carlos.ortega@doublevpartners.com>
2025-07-23 17:12:36 -0500

    Añadido readme con explicaciones

--- src/environments/environment.beta.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,
-    baseUrl: 'https://api-beta.corplan.com.ar/v1',
-    authenticationApiUrl: 'https://api-beta.corplan.com.ar/v1/auth',
-    googleMapsApiKey: 'AIzaSyDX2rvAaJPOOacu98LStnurwe0-tBaMyYs',
-    enableNgrokSkip: false, // Deshabilitado en beta (usar SSL normal)
-};
--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: true,
-    baseUrl: 'https://corplan-api.corplan.com/v1',
-    authenticationApiUrl: 'https://corplan-api.corplan.com/v1/auth',
-    googleMapsApiKey: 'AIzaSyDX2rvAaJPOOacu98LStnurwe0-tBaMyYs',
-    enableNgrokSkip: false, // Deshabilitado en producción
-};
