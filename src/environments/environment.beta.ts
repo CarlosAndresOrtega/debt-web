f4cdd88c0b42469e972f2f760c37fd2a0eca9b54
CarlosOrtegaDoubleV <carlos.ortega@doublevpartners.com>
2025-07-23 14:32:04 -0500

    first commit

---
+++ src/environments/environment.beta.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    baseUrl: 'https://api-beta.corplan.com.ar/v1',
+    authenticationApiUrl: 'https://api-beta.corplan.com.ar/v1/auth',
+    googleMapsApiKey: 'AIzaSyDX2rvAaJPOOacu98LStnurwe0-tBaMyYs',
+    enableNgrokSkip: false, // Deshabilitado en beta (usar SSL normal)
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    baseUrl: 'https://corplan-api.corplan.com/v1',
+    authenticationApiUrl: 'https://corplan-api.corplan.com/v1/auth',
+    googleMapsApiKey: 'AIzaSyDX2rvAaJPOOacu98LStnurwe0-tBaMyYs',
+    enableNgrokSkip: false, // Deshabilitado en producción
+};
