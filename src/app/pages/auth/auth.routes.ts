0984c3d69fe2dca0798fee8a1f642f665567f190
CarlosOrtegaDoubleV <carlos.ortega@doublevpartners.com>
2026-01-14 15:45:17 -0500

    Add registration feature and update authentication flow

--- src/app/pages/auth/auth.routes.ts
+++ src/app/pages/auth/auth.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AccessDenied } from './accessdenied';
 import { Error } from './error';
 import { Login } from './login';
+import { Register } from './register';
 
 export default [
     { path: 'error', component: Error },
     { path: 'access', component: AccessDenied },
     { path: 'login', component: Login },
+    { path: 'register', component: Register },
     { path: '**', redirectTo: '/notfound' },
 ] as Routes;
