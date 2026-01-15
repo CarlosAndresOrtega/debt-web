4de0e1754ec3ddadf64fd1df8c20c09d00246960
CarlosOrtegaDoubleV <carlos.ortega@doublevpartners.com>
2026-01-15 09:41:27 -0500

    Add dashboard component and update routing to include dashboard

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { DebtsContainer } from './debt/container';
+import { DebtsStatsComponent } from './dashboard/container';
 
 export default [
     { path: '', redirectTo: 'debts', pathMatch: 'full' },
     { path: 'debts', data: { breadcrumb: 'Debts' }, component: DebtsContainer },
+    { path: 'dashboard', data: { breadcrumb: 'Dashboard' }, component: DebtsStatsComponent },
+
 ] as Routes;
